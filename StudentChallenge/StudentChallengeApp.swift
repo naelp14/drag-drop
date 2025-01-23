1dff057f731a55be5bd87fd985aff7827a439a07
Nathaniel Putera <nathaniel.putera@framna.com>
2025-01-23 09:15:27 +0100

    Initial Commit

---
+++ StudentChallenge/StudentChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentChallengeApp.swift
+//  StudentChallenge
+//
+//  Created by Nathaniel Putera on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct StudentChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
